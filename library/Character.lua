8b0911d7284a8180579a8666579fa8db2a3c99a9
thejimbo27 <thejimbo27@fishhousemafia.com>
2025-01-24 21:21:50 -0500

    move charactoer code into character class

--- library/Character.lua
+++ library/Character.lua
@@ -1,5 +1,8 @@
-local Weapon = require("library/Weapon")
+local ServiceLocator = require("library/ServiceLocator")
 local Vector2 = require("library/Vector2")
+local Weapon = require("library/Weapon")
+
+local eventBus = ServiceLocator:get("EventBus")
 
 ---@class Character
 ---@field private __index Character
@@ -14,12 +17,36 @@ function Character.new(position, weapon)
   local self = setmetatable({}, Character)
   self.position = position or Vector2.zero()
   self.weapon = weapon or Weapon.new(self)
+
+  eventBus:subscribe("update", function(dt) self:onUpdate(dt) end)
+
   return self
 end
 
----@param moveVector Vector2
-function Character:move(moveVector)
-  self.position = self.position + moveVector
+function Character:onUpdate(dt)
+  local speed = 60
+  local move = Vector2.zero()
+  if love.keyboard.isDown("w") then
+    move = move + Vector2.new(0, -1)
+  end
+  if love.keyboard.isDown("a") then
+    move = move + Vector2.new(-1, 0)
+  end
+  if love.keyboard.isDown("s") then
+    move = move + Vector2.new(0, 1)
+  end
+  if love.keyboard.isDown("d") then
+    move = move + Vector2.new(1, 0)
+  end
+  if love.mouse.isDown(1) then
+    eventBus:emit("attackRequest", {
+      source = self.position,
+      destination = Vector2.new(love.mouse.getPosition()) / _G.SCALE_FACTOR,
+      self.weapon,
+    })
+  end
+
+  self.position = self.position + move:unit() * (speed * dt)
 end
 
 return Character
--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
   end
 end
 
+_G.SCALE_FACTOR = 1
 _G.VEC_COUNT = 0
 
 local Character = require("library/Character")
@@ -24,21 +25,16 @@ local messageQueue = ServiceLocator:register(MessageQueue)
 local projectileManager = ServiceLocator:register(ProjectileManager)
 local hero = nil ---@type Character
 local lastAttack = nil
-local scaleFactor = 1
 --local frameRate = 60
 
 function love.load()
   local gameDimensions = Vector2.new(320, 240)
   local desktopDimensions = Vector2.new(love.window.getDesktopDimensions(1))
   local scaled = (desktopDimensions / gameDimensions):floor()
-  scaleFactor = math.min(scaled:tuple()) - 1
-  love.window.setMode((gameDimensions * scaleFactor):tuple())
+  _G.SCALE_FACTOR = math.min(scaled:tuple()) - 1
+  love.window.setMode((gameDimensions * _G.SCALE_FACTOR):tuple())
 
   hero = Character.new(gameDimensions / 2)
-
-  eventBus:subscribe("moveRequest", function(moveVector)
-    hero:move(moveVector)
-  end)
 
   eventBus:subscribe("attackRequest", function(attackVectors)
     lastAttack = attackVectors
@@ -57,32 +53,7 @@ function love.keypressed(key)
 end
 
 function love.update(dt)
-  local speed = 60
-  if love.keyboard.isDown("w") then
-    messageQueue:push("moveRequest", Vector2.new(0, -1))
-  end
-  if love.keyboard.isDown("a") then
-    messageQueue:push("moveRequest", Vector2.new(-1, 0))
-  end
-  if love.keyboard.isDown("s") then
-    messageQueue:push("moveRequest", Vector2.new(0, 1))
-  end
-  if love.keyboard.isDown("d") then
-    messageQueue:push("moveRequest", Vector2.new(1, 0))
-  end
-  if love.mouse.isDown(1) then
-    eventBus:emit("attackRequest", {
-      source = hero.position,
-      destination = Vector2.new(love.mouse.getPosition()) / scaleFactor,
-    })
-  end
-
-  local moveVector = Vector2.zero()
-  for message in messageQueue:collect("moveRequest") do
-    moveVector = moveVector + message
-  end
-
-  eventBus:emit("moveRequest", moveVector:unit() * (speed * dt))
+  eventBus:emit("update", dt)
 
   projectileManager:evaluate(dt)
 
@@ -93,20 +64,20 @@ end
 
 function love.draw()
   love.graphics.setColor(1, 1, 1)
-  local x, y = (hero.position:floor() * scaleFactor):tuple()
-  local w, h = (Vector2.new(8, 8) * scaleFactor):tuple()
+  local x, y = (hero.position:floor() * _G.SCALE_FACTOR):tuple()
+  local w, h = (Vector2.new(8, 8) * _G.SCALE_FACTOR):tuple()
   love.graphics.rectangle("fill", x, y, w, h)
 
   love.graphics.setColor(1, 0, 0)
   if lastAttack then
-    local x1, y1 = (lastAttack.source:floor() * scaleFactor):tuple()
-    local x2, y2 = (lastAttack.destination:floor() * scaleFactor):tuple()
+    local x1, y1 = (lastAttack.source:floor() * _G.SCALE_FACTOR):tuple()
+    local x2, y2 = (lastAttack.destination:floor() * _G.SCALE_FACTOR):tuple()
     love.graphics.line(x1, y1, x2, y2)
   end
 
   for projectile in projectileManager:iterate() do
-    x, y = (projectile.position:floor() * scaleFactor):tuple()
-    w, h = scaleFactor, scaleFactor
+    x, y = (projectile.position:floor() * _G.SCALE_FACTOR):tuple()
+    w, h = _G.SCALE_FACTOR, _G.SCALE_FACTOR
     love.graphics.rectangle("fill", x, y, w, h)
   end
 
